deda9dc92de3e916a3d561e7e7e7ac77730cf253
LDmitriy <uks.loboyko@gmail.com>
2022-04-27 22:13:18 +0300

    Add templates

--- app/currency/models.py
+++ app/currency/models.py
@@ -5,3 +5,11 @@ class ContactUs(models.Model):
     email_from = models.CharField(max_length=50)
     subject = models.CharField(max_length=40)
     message = models.TextField(max_length=3000)
+
+
+class Rate(models.Model):
+    type = models.CharField(max_length=3)
+    source = models.CharField(max_length=50)
+    created = models.DateTimeField()
+    buy = models.DecimalField(max_digits=10, decimal_places=2)
+    sale = models.DecimalField(max_digits=10, decimal_places=2)
