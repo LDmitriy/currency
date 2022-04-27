9e3335eb460c3701d1878f7ca19c4f3a03818ad4
LDmitriy <uks.loboyko@gmail.com>
2022-04-27 22:18:19 +0300

    Add templates, and fix flake8

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,14 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 from currency.models import ContactUs, Rate
 
 
 def contactus(request):
-    # li = []
-    # for contact in ContactUs.objects.all():
-    #     li.append((contact.id, contact.email_from, contact.subject, contact.message))
-    # return HttpResponse(str(li))
     contacts = ContactUs.objects.all()
     return render(request, 'contactus.html', context={'contacts': contacts})
 
