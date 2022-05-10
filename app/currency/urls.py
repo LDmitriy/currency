dee6194981fa8c99c43d1c8d7c78f1e9d8b36a41
LDmitriy <uks.loboyko@gmail.com>
2022-05-10 21:02:18 +0300

    HW9 Emails

--- app/currency/urls.py
+++ app/currency/urls.py
@@ -5,6 +5,7 @@ app_name = 'currency'
 
 urlpatterns = [
     path('ContactUs/', currency_views.ContactUsList.as_view(), name='contact_us'),
+    path('contactus_create/', currency_views.ContactUsCreate.as_view(), name='contactus_create'),
     path('Rates/', currency_views.RatesList.as_view(), name='rates'),
     path('Source/', currency_views.SourceList.as_view(), name='source'),
     path('source/create/', currency_views.SourceCreate.as_view(), name='source_create'),
